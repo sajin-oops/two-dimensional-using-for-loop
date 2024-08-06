4449c68fd671a8a45c6d32fc52e94aa5024e89dc
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-06 21:25:12 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+groceories = [["apple","mango","orange"],["carrot","potato","onion"]
+              ,["fish","chicken","beaf"]]
+
+print(groceories)
+
+#O/P -[['apple', 'mango', 'orange'], ['carrot', 'potato', 'onion'], ['fish', 'chicken', 'beaf']]
+
+for collection in groceories:
+    print(collection)
+
+
+#O/P
+
+'''
+['apple', 'mango', 'orange']
+['carrot', 'potato', 'onion']
+['fish', 'chicken', 'beaf']
+'''
